598b8972a89a0995c62ffde31349e98383a2864d
Justin <justinjb1355@gmail.com>
2026-07-14 19:39:28 +0700

    fix: handle OPTIONS preflight for CORS

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors({
+const corsOptions = {
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
 
 // Routes
